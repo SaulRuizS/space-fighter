a6a73cc8a44f1be91c118a82ddf93bc2c30b7183
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-07-05 16:45:38 -0500

    MovingBG first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import MovingBG from './components/MovingBG';
 import MainMenu from './sections/MainMenu';
 import './styles/global.scss'
 
 const App = () => {
     return (
         <div>
-            <MainMenu />
+            <MovingBG />
+            {/* <MainMenu /> */}
         </div>
     );
 };
--- src/components/MovingBG.jsx
+++ src/components/MovingBG.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import '../styles/MovingBG.scss';
 
 const MovingBG = () => {
+
+    
+
     return (
-        <div>
-            
+        <div className='moving-bg'>
+            <div className='moving-bg__small-stars'>
+                <div className='star small'></div>
+                <div className='star small'></div>
+                <div className='star small'></div>
+            </div>
+            <div className='moving-bg__medium-stars'>
+                <div className='star medium'></div>
+                <div className='star medium'></div>
+                <div className='star medium'></div>
+            </div>
         </div>
     );
 };
